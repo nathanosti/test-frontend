1c063e15c0673053962b2791e6772c6f444c8dc8
Nathan Osti <nathanosti.job@gmail.com>
2022-07-04 13:46:14 -0300

    🐛 Feat: fix unecessary load when change route

--- src/components/SideBar/NavItem.tsx
+++ src/components/SideBar/NavItem.tsx
@@ -1,39 +1,45 @@
-import { Link, Flex, Icon } from "@chakra-ui/react";
+import Link from "next/link";
+import { Link as ChakraLink, Flex, Icon } from "@chakra-ui/react";
 import { NavItemProps } from "../../types/SideBarTypes";
 
-
-export default function NavItem({ icon, href, children, ...rest }: NavItemProps) {
+export default function NavItem({
+  icon,
+  href,
+  children,
+  ...rest
+}: NavItemProps) {
   return (
-    <Link
-      href={href}
-      style={{ textDecoration: "none" }}
-      _focus={{ boxShadow: "none" }}
-    >
-      <Flex
-        align="center"
-        p="4"
-        mx="4"
-        borderRadius="lg"
-        role="group"
-        cursor="pointer"
-        _hover={{
-          bg: "#38A169",
-          color: "white",
-        }}
-        {...rest}
+    <Link href={href}>
+      <ChakraLink
+        style={{ textDecoration: "none" }}
+        _focus={{ boxShadow: "none" }}
       >
-        {icon && (
-          <Icon
-            mr="4"
-            fontSize="16"
-            _groupHover={{
-              color: "white",
-            }}
-            as={icon}
-          />
-        )}
-        {children}
-      </Flex>
+        <Flex
+          align="center"
+          p="4"
+          mx="4"
+          borderRadius="lg"
+          role="group"
+          cursor="pointer"
+          _hover={{
+            bg: "#38A169",
+            color: "white",
+          }}
+          {...rest}
+        >
+          {icon && (
+            <Icon
+              mr="4"
+              fontSize="16"
+              _groupHover={{
+                color: "white",
+              }}
+              as={icon}
+            />
+          )}
+          {children}
+        </Flex>
+      </ChakraLink>
     </Link>
   );
-};
+}
